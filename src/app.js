cf9c611bbe6a50849623a592e5bbdb8c18998e3c
crissttofer <crissttofer2021@gmail.com>
2024-04-23 09:03:34 -0500

    modificaciones y depencias

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import mysql from "mysql2";
+import morgan from "morgan";
 
 const app=express();
 const PORT=4000;
 
 app.set('port',PORT)
+app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
-  res.send('Yherson cabro')
+  res.send('hola')
 })
 app.listen(PORT,()=>{
   console.log(`Listening on http://localhost:${PORT}`)
