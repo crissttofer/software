436e8a8f3e127f0e063d33012cc1aa05f368450e
samir <jhonergomez20@gmail.com>
2024-06-23 19:55:29 -0500

    posible manejo de datos

--- src/controllers/viewPrueba.js
+++ src/controllers/viewPrueba.js
@@ -1,5 +1,16 @@
+
+const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
 function view(req,res){
-  res.render(req.params.id)
+  const title = 
+  req.params.id === 'home' ? 'Home' :
+  req.params.id === 'login.hbs' ? 'Login' :
+   'Error';
+
+  res.render(req.params.id, {
+    title: title,
+    item: items
+  });
 }
 
 export {
