d05dddc5a9d3b35639816afb5dd05e03db41c9b5
samir <jhonergomez20@gmail.com>
2024-06-26 23:29:34 -0500

    Registro

--- src/controllers/viewPrueba.js
+++ src/controllers/viewPrueba.js
@@ -1,10 +1,11 @@
 
-const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4','Item 5','Item 6'];
 
 function view(req,res){
   const title = 
   req.params.id === 'home' ? 'Home' :
-  req.params.id === 'login.hbs' ? 'Login' :
+  req.params.id === 'login' ? 'Login':
+  req.params.id === 'evento' ? 'Evento':
    'Error';
 
   res.render(req.params.id, {
